f2d6d4ed231a714ff9aa5571ae16e98cd0d9a7b1
TheThomasY <twy1g16@soton.ac.uk>
2021-12-03 18:28:32 +0000

    style: rules page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React, { useState } from 'react';
 import ScoreCard from './components/ScoreCard';
 import GameBoard from './components/GameBoard';
+import Rules from './components/Rules';
 import './App.css';
 
 function App() {
+  const [showRules, setShowRules] = useState(false);
+
+  const toggleRules = () => {
+    showRules ? setShowRules(false) : setShowRules(true);
+  };
+
   return (
     <div>
       <ScoreCard />
       <GameBoard />
+      <Rules onToggleRules={toggleRules} showRules={showRules} />
     </div>
   );
 }
---
+++ src/components/Rules.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './SCSS/Rules.css';
+import rulesImg from '../assets/images/image-rules.svg';
+import rulesCloseIcon from '../assets/images/icon-close.svg';
+
+export default function Rules(props) {
+  let showRules = props.showRules;
+
+  const toggleRulesHandler = () => {
+    props.onToggleRules();
+  };
+
+  return (
+    <div>
+      {!showRules ? (
+        <div onClick={toggleRulesHandler} className='rules-btn'>
+          RULES
+        </div>
+      ) : (
+        <div className='rules-full-page'>
+          <div className='rules-fp-title'>RULES</div>
+          <img
+            className='rules-fp-img'
+            src={rulesImg}
+            alt='Image showing the rules of rock paper scissors'
+          />
+          <img
+            onClick={toggleRulesHandler}
+            className='rules-fp-close'
+            src={rulesCloseIcon}
+            alt='Close rules button'
+          />
+        </div>
+      )}
+    </div>
+  );
+}
