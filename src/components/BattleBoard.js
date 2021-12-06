6afeb843f4410e6ac18612f05025b1be4cb6c8dd
TheThomasY <twy1g16@soton.ac.uk>
2021-12-06 17:45:36 +0000

    style: fixed some placement on empty shadow, refactored out icon type

--- src/components/BattleBoard.js
+++ src/components/BattleBoard.js
@@ -1,21 +1,27 @@
 import './SCSS/BattleBoard.css';
-import paperIcon from '../assets/images/icon-paper.svg';
-import scissorsIcon from '../assets/images/icon-scissors.svg';
-import rockIcon from '../assets/images/icon-rock.svg';
 
 import CreateAnIcon from '../components/pieces/CreateAnIcon';
 
 export default function PlayedBoard() {
+  let playerPick = 'rock';
+  let housePick = '';
+
   return (
     <div className='battle-board'>
       <div className='bb-picked bb-you-picked'>
-        {/* <div className='bb-empty'></div> */}
-        <CreateAnIcon iconType={'rock'} />
+        {playerPick === '' ? (
+          <div className='bb-empty'></div>
+        ) : (
+          <CreateAnIcon iconType={playerPick} />
+        )}
         <div className='picked-by-label'>You Picked</div>
       </div>
       <div className='bb-picked bb-house-picked'>
-        {/* <div className='bb-empty'></div> */}
-        <CreateAnIcon iconType={'paper'} />
+        {housePick === '' ? (
+          <div className='bb-empty'></div>
+        ) : (
+          <CreateAnIcon iconType={housePick} />
+        )}
         <div className='picked-by-label'>The House Picked</div>
       </div>
     </div>
