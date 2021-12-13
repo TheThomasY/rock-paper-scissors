f10d6189901f36a515253b7730e3912bf1e66784
TheThomasY <twy1g16@soton.ac.uk>
2021-12-13 18:54:24 +0000

    style: added fade in animation to house pick and result

--- src/components/BattleBoard.js
+++ src/components/BattleBoard.js
@@ -39,11 +39,7 @@ export default function PlayedBoard(props) {
     <div className='battle-board-and-result'>
       <div className='battle-board'>
         <div className='bb-picked bb-you-picked'>
-          {playerPick === '' ? (
-            <div className='bb-empty'></div>
-          ) : (
-            <CreateAnIcon iconType={playerPick} />
-          )}
+          <CreateAnIcon iconType={playerPick} />
           <div className='picked-by-label'>You Picked</div>
         </div>
         <div className='result-div'>
@@ -53,11 +49,8 @@ export default function PlayedBoard(props) {
           </div>
         </div>
         <div className='bb-picked bb-house-picked'>
-          {housePick === '' ? (
-            <div className='bb-empty'></div>
-          ) : (
-            <CreateAnIcon iconType={housePick} />
-          )}
+          <div className='bb-empty'></div>
+          <CreateAnIcon iconType={housePick} />
           <div className='picked-by-label'>The House Picked</div>
         </div>
       </div>
